01b9ccdb55fde4f56d049c98be56fd87b2878422
Zoravur Singh <zoravur.s@gmail.com>
2024-09-12 10:39:46 -0400

    commit to reintroduce LSP

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-	cout << "Hello, world!" << endl;
-	return 0;
+    vector<int> v;
+    int i;
+    while (cin >> i) v.push_back(i);
+
+    std::sort(v.begin(), v.end());
+    
+
+    for (auto& i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
+
